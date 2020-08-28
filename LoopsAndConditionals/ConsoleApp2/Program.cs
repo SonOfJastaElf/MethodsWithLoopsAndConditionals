c3e268b33b21325ddbc34fdb74096e2c9b11d883
Brian Henderson <henderson.brian82@gmail.com>
2020-08-28 17:12:26 -0400

    Final Revisions

--- LoopsAndConditionals/ConsoleApp2/Program.cs
+++ LoopsAndConditionals/ConsoleApp2/Program.cs
@@ -6,90 +6,101 @@ namespace MethodExercise3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Here's a series of methods I've made to practice coding in C#.  Enjoy!");
-        }
-        public static void CountDownByOnes()
-        {
-            int target = -1000;
-            for (int i = 1000; i >= target; i--)
-            {
-                Console.WriteLine(i);
-            }
-        }
 
-        public static void CountUpByThrees()
-        {
-            for (int n = 3; n <= 999; n += 3)
-            {
-                Console.WriteLine(n);
-            }
+            var target = -1000;
+            CountDownByOnes(target);
 
-        }
+            CountUpByThrees();
 
-        public static bool AreTheseEqual()
-        {
             Console.WriteLine("Now put in two numbers, and see if they're equal");
-            int a = int.Parse(Console.ReadLine());
-            int b = int.Parse(Console.ReadLine());
+            var userInput1 = Console.ReadLine();
+            var userInput2 = Console.ReadLine();
+            var a = int.Parse(userInput1);
+            var b = int.Parse(userInput2);
+            AreTheseEqual(a, b);
 
-            if (a == b)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public static void VotingAge()
-        {
             Console.WriteLine("Are you old enough to vote?");
+            var userInput3 = Console.ReadLine();
+            var voteAge = int.Parse(userInput3);
+            VotingAge(voteAge);
 
-            int vote = int.Parse(Console.ReadLine());
-
-            if (vote >= 18)
-            {
-                Console.WriteLine("Congratulations, you're old enough to vote!  See you at the polls?");
-            }
-            else
-            {
-                Console.WriteLine("Sorry, you're not old enough to vote yet.  Try again in a few years.");
-            }
-        }
-
-        public static void EvenOrOdd()
-        {
             Console.WriteLine("Give me a different number and let's see if it's even or odd");
-            int c = int.Parse(Console.ReadLine());
-            
-            if (c % 2 == 0)
-            {
-                Console.WriteLine ($"{c} is even!");
-            }
-            else
-            {
-                Console.WriteLine($"{c} is odd!");
-            }
-        }
-
-        public static void IsThisPositive()
-        {
+            var userInput4 = Console.ReadLine();
+            var c = int.Parse(userInput4);
+            EvenOrOdd(c);
+ 
             Console.WriteLine("Put in yet another number");
-            int d = int.Parse(Console.ReadLine());
+            var userInput5 = Console.ReadLine();
+            var d = int.Parse(userInput5);
+            IsThisPositive(d);
 
-           if (d > 0)
-            {
-                Console.WriteLine($"{d} is positive");
-            }
-            else if (d == 0)
-            {
-                Console.WriteLine("0 is neither positive nor negative");
-            }
-            else
-            {
-                Console.WriteLine($"{d} is negative");
-            }
         }
+
+        public static void CountDownByOnes(int target)
+            {
+                for (int i = 1000; i >= target; i--)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            public static void CountUpByThrees()
+            {
+                for (int n = 3; n <= 999; n += 3)
+                {
+                    Console.WriteLine(n);
+                }
+
+            }
+
+            public static bool AreTheseEqual(int a, int b)
+            {
+                if (a == b)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            public static void VotingAge(int voteAge)
+            {
+                if (voteAge >= 18)
+                {
+                    Console.WriteLine("Congratulations, you're old enough to vote!  See you at the polls?");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you're not old enough to vote yet.  Try again in a few years.");
+                }
+            }
+            public static void EvenOrOdd(int c)
+            {
+                if (c % 2 == 0)
+                {
+                    Console.WriteLine($"{c} is even!");
+                }
+                else
+                {
+                    Console.WriteLine($"{c} is odd!");
+                }
+            }
+
+            public static void IsThisPositive(int d)
+            {
+                if (d > 0)
+                {
+                    Console.WriteLine($"{d} is positive");
+                }
+                else if (d == 0)
+                {
+                    Console.WriteLine("0 is neither positive nor negative");
+                }
+                else
+                {
+                    Console.WriteLine($"{d} is negative");
+                }
+            }
     }
 }
